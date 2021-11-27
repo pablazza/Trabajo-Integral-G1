532af539a1c6a318d464613a0fd4dce3965890fe
pablazza <pzunigan@utem.cl>
2021-11-27 20:15:56 -0300

    se avanzo en el desarrollo del entorno web de la aplicacion y se agregaron las librerias correspondientes para su ejecucion

---
+++ javascript/aplicacion.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // Pantallas mas grandes que >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // Pantalla mas grande que >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
